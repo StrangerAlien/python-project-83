d6d036a489654c176e22453b240e44b1904a3c7b
aleksei <jiunov@gmail.com>
2024-06-25 22:09:50 +0600

    add url to db

--- page_analyzer/actions_with_db.py
+++ page_analyzer/actions_with_db.py
@@ -6,13 +6,15 @@ from datetime import datetime
 def connect_db():
     try:
         conn = psycopg2.connect(DATABASE_URL)
-        return conn
-    except:
-        print('Can`t establish connection to database')
+    except Exception as error:
+        # print('Can`t establish connection to database')
+        raise error
+    return conn
 
 
 def save_url(url):
     conn = connect_db()
     with conn.cursor() as curs:
-        curs.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s);', (url, datetime.now()))
+        curs.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s);',
+                     (url, datetime.now()))
         conn.commit()
